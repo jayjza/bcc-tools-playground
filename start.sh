c327107479de90d2d0af4f3e3e9d4664240f6308
Jason Bissict <jason@thinkst.com>
2023-09-29 09:35:16 +0200

    Neaten up

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ sudo docker run -ti --rm\
     --network=host \
     --privileged \
     -v /etc/localtime:/etc/localtime:ro \
-    -v ./custom-bcc-tools:/bcc-playground/custom-bcc-tools bcc-playground
+    -v ./custom-bcc-tools:/bcc-playground/custom-bcc-tools \
+    bcc-playground
