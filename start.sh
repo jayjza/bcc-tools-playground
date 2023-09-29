976541e40ac05a14b812664e4d3a39ae972f200d
Jason Bissict <jason@thinkst.com>
2023-09-29 10:56:12 +0200

    Fix missing kprobe_events file

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@ sudo docker run -ti --rm\
     --cap-add=NET_ADMIN \
     --network=host \
     --privileged \
+    --name bcc \
+    --pid=host \
+    -v /sys/kernel/debug:/sys/kernel/debug:rw \
     -v /etc/localtime:/etc/localtime:ro \
     -v ./custom-bcc-tools:/bcc-playground/custom-bcc-tools \
     bcc-playground
